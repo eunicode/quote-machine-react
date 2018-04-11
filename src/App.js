b324ad54f9ac3e2daebaf415e977048ba67392d9
Eunice Park <28845173+eunipa@users.noreply.github.com>
2018-04-10 22:26:57 -0700

    Add JSX skeleton

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <h1>
+          Game of Thrones Quote Machine
+        </h1>
+        
+        <div className="Quote-box">
+         <div className="Quote-display">
+         Click button to generate a random Game of Thrones quote.
+        </div>
+        </div>
+
+        <div className="Action-box">
+        <button onClick="newQuote">
+          Change quote
+          </button>
+          <button onClick="tweetQuote">
+            Tweet quote
+            </button>           
+        </div>
+
+        </div>
     );
   }
 }
