159d4d64504fe8183274ec3db0a943acb3104533
Eunice Park <28845173+eunipa@users.noreply.github.com>
2018-04-13 14:33:16 -0700

    Add change quote, tweet quote functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // import logo from './logo.svg';
-import "./App.css";
+import './App.css';
 
+/*---------- GLOBAL ----------*/
+
+const QUOTES = [
+  `"When you play a game of thrones, you win or you die." ― Cersei Lannister`,
+  `"Crowns do queer things to the heads beneath them." ― Tyrion Lannister`,
+  `"My skin has turned to porcelain, to ivory, to steel." ― Sansa Stark`,
+  `"Stick em with the pointy end." ― Arya Stark`,
+  `"Never forget what you are, for surely the world will not. Make it your strength. Then it can never be your weakness. Armour yourself in it, and it will never be used to hurt you." ― Tyrion Lannister`,
+  `"When the snows fall and the white winds blow, the lone wolf dies but the pack survives." ― Eddard Stark`,
+  `"A reader lives a thousand lives before he dies. The man who never reads lives only one." ― Jojen Reed`,
+  `"Summer will end soon enough, and childhood as well." ― Eddard Stark`,
+  `"What is dead may never die, but rises again, harder and stronger." ― Drowned God Priest`,
+  `"I have a tender spot in my heart for cripples and bastards and broken things." ― Tyrion Lannister`,
+];
+
+let randomNumber = 0;
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: QUOTES[randomNumber],
+    };
+  }
+
+  changeQuote = () => {
+    randomNumber = Math.floor(Math.random() * QUOTES.length);
+    this.setState({ quote: QUOTES[randomNumber] });
+    console.log(this.state.quote);
+  };
+
+  tweetQuote() {
+    window.open(
+      'https://twitter.com/intent/tweet?text=' + QUOTES[randomNumber]
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,8 +45,11 @@ class App extends Component {
         <header className="App-header">
           <h1>Game of Thrones Quote Machine</h1>
         </header>
-        <QuoteBox />
-        <ButtonBox />
+        <QuoteBox quote={this.state.quote} />
+        <ButtonBox
+          changeQuote={this.changeQuote}
+          tweetQuote={this.tweetQuote}
+        />
         <Footer />
       </div>
     );
@@ -19,19 +57,14 @@ class App extends Component {
 }
 
 const QuoteBox = props => {
-  return (
-    <main className="Quote-box">
-      "When you play the game of thrones, you win or you die." ― Cersei
-      Lannister
-    </main>
-  );
+  return <main className="Quote-box">{props.quote}</main>;
 };
 
 const ButtonBox = props => {
   return (
     <div className="Button-box">
-      <button onClick="changeQuote">Change quote</button>
-      <button onClick="tweetQuote">Tweet quote</button>
+      <button onClick={props.changeQuote}>Change quote</button>
+      <button onClick={props.tweetQuote}>Tweet quote</button>
     </div>
   );
 };
